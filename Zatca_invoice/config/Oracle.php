2d53f043170f5ed25bca8b12e4b5a39c1bc5c8ce
AGHGroup <it_dev@aghgroup.com>
2026-06-09 15:02:00 +0300

    begin zatcaPHP laravel

---
+++ Zatca_invoice/config/Oracle.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+public function getConnection()
+    {
+        return $this->connection;
+    }
+
+    public function setConnection($connection)
+    {
+        $this->connection = $connection;
+    }
+
+
+
+    ?>
