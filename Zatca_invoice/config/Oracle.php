891cecb6befe9c3ecc7cfa21fd095bb5b0e01e0f
AGHGroup <it_dev@aghgroup.com>
2026-06-09 16:49:12 +0300

    Oracle connection

--- Zatca_invoice/config/Oracle.php
+++ Zatca_invoice/config/Oracle.php
@@ -1,17 +1,12 @@
 <?php
 
-
-
-public function getConnection()
-    {
-        return $this->connection;
-    }
-
-public function setConnection($connection)
-    {
-        $this->connection = $connection;
-    }
-
-
-
-    ?>
+return [
+    'driver' => env('ORACLE_DRIVER', 'oracle'),
+    'host' => env('ORACLE_HOST', ''),
+    'port' => env('ORACLE_PORT', '1521'),
+    'database' => env('ORACLE_DATABASE', ''),
+    'username' => env('ORACLE_USERNAME', ''),
+    'password' => env('ORACLE_PASSWORD', ''),
+    'charset' => env('ORACLE_CHARSET', 'AL32UTF8'),
+    'prefix' => '',
+];
